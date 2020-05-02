88f892e524726d26faf4acb234e01c303538e31d
Jaliya Lamahewa <jaliya@pickme.lk>
2020-05-02 19:00:15 +0530

    without gorutine

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 import "time"
 
 type employee struct {
@@ -23,12 +26,22 @@ func main() {
 	startTime := time.Now()
 	fmt.Println("startTime time  ", startTime)
 	//emps := []employee{}
-	emps := make([]employee, 10000)
+	emps := make([]employee, 0)
+
+	for i := 0; i < 100000; i++ {
+		emp := employee{
+			name:   "wewewe",
+			basic:  rand.Intn(100000),
+			otRate: rand.Intn(1000),
+			otHrs:  rand.Intn(100),
+		}
+		emps = append(emps, emp)
+	}
 
 	for _, em := range emps {
 		otAmount := calOTAmount(em.otRate, em.otHrs)
 		fullSalary := calfullSalary(em.basic, otAmount)
-		fmt.Println(em.name, "Full Salary: ", fullSalary)
+		fmt.Println(em.name, "| basic:", em.basic, "| otRate:", em.otRate, "| otHrs:", em.otHrs, "| Full Salary:", fullSalary)
 		//time.Sleep(1 * time.Second)
 	}
 	endTime := time.Now()

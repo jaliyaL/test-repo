b670d3e49f2b149de5ae20873228cf7ed0ccf0ba
Jaliya Lamahewa <jaliya@pickme.lk>
2020-05-02 14:16:31 +0530

    gorutines - hardcored employees

--- main.go
+++ main.go
@@ -10,14 +10,13 @@ type employee struct {
 	otHrs  int
 }
 
-func calfullSalary(basic int, otAmount int) int {
-	return basic + otAmount
+func calfullSalary(basic int, otAmount int, fulSal chan int) {
+	fulSal <- basic + otAmount
 }
 
-func calOTAmount(otRate int, otHrs int) int {
+func calOTAmount(otRate int, otHrs int, otAmCh chan int) {
 	//time.Sleep(1 * time.Second)
-	return otRate * otHrs
-
+	otAmCh <- otRate * otHrs
 }
 
 func main() {
@@ -26,227 +25,10 @@ func main() {
 	emps := []employee{}
 
 	emps = append(emps, employee{
-		name:   "Saman",
-		basic:  100000,
+		name:   "Samann",
+		basic:  20000,
 		otRate: 500,
-		otHrs:  20,
-	})
-	emps = append(emps, employee{
-		name:   "Gasalu",
-		basic:  50000,
-		otRate: 300,
-		otHrs:  40,
-	})
-	emps = append(emps, employee{
-		name:   "Raju",
-		basic:  45000,
-		otRate: 700,
-		otHrs:  40,
-	})
-	emps = append(emps, employee{
-		name:   "ko",
-		basic:  500000,
-		otRate: 700,
-		otHrs:  20,
-	})
-	emps = append(emps, employee{
-		name:   "roja",
-		basic:  60000,
-		otRate: 600,
-		otHrs:  30,
-	})
-	emps = append(emps, employee{
-		name:   "konara",
-		basic:  25000,
-		otRate: 700,
-		otHrs:  40,
-	})
-
-	emps = append(emps, employee{
-		name:   "Saman",
-		basic:  100000,
-		otRate: 500,
-		otHrs:  20,
-	})
-	emps = append(emps, employee{
-		name:   "Gasalu",
-		basic:  50000,
-		otRate: 300,
-		otHrs:  40,
-	})
-	emps = append(emps, employee{
-		name:   "Raju",
-		basic:  45000,
-		otRate: 700,
-		otHrs:  40,
-	})
-	emps = append(emps, employee{
-		name:   "ko",
-		basic:  500000,
-		otRate: 700,
-		otHrs:  20,
-	})
-	emps = append(emps, employee{
-		name:   "roja",
-		basic:  60000,
-		otRate: 600,
-		otHrs:  30,
-	})
-	emps = append(emps, employee{
-		name:   "konara",
-		basic:  25000,
-		otRate: 700,
-		otHrs:  40,
-	})
-	emps = append(emps, employee{
-		name:   "Saman",
-		basic:  100000,
-		otRate: 500,
-		otHrs:  20,
-	})
-	emps = append(emps, employee{
-		name:   "Gasalu",
-		basic:  50000,
-		otRate: 300,
-		otHrs:  40,
-	})
-	emps = append(emps, employee{
-		name:   "Raju",
-		basic:  45000,
-		otRate: 700,
-		otHrs:  40,
-	})
-	emps = append(emps, employee{
-		name:   "ko",
-		basic:  500000,
-		otRate: 700,
-		otHrs:  20,
-	})
-	emps = append(emps, employee{
-		name:   "roja",
-		basic:  60000,
-		otRate: 600,
-		otHrs:  30,
-	})
-	emps = append(emps, employee{
-		name:   "konara",
-		basic:  25000,
-		otRate: 700,
-		otHrs:  40,
-	})
-	emps = append(emps, employee{
-		name:   "Saman",
-		basic:  100000,
-		otRate: 500,
-		otHrs:  20,
-	})
-	emps = append(emps, employee{
-		name:   "Gasalu",
-		basic:  50000,
-		otRate: 300,
-		otHrs:  40,
-	})
-	emps = append(emps, employee{
-		name:   "Raju",
-		basic:  45000,
-		otRate: 700,
-		otHrs:  40,
-	})
-	emps = append(emps, employee{
-		name:   "ko",
-		basic:  500000,
-		otRate: 700,
-		otHrs:  20,
-	})
-	emps = append(emps, employee{
-		name:   "roja",
-		basic:  60000,
-		otRate: 600,
-		otHrs:  30,
-	})
-	emps = append(emps, employee{
-		name:   "konara",
-		basic:  25000,
-		otRate: 700,
-		otHrs:  40,
-	})
-	emps = append(emps, employee{
-		name:   "Saman",
-		basic:  100000,
-		otRate: 500,
-		otHrs:  20,
-	})
-	emps = append(emps, employee{
-		name:   "Gasalu",
-		basic:  50000,
-		otRate: 300,
-		otHrs:  40,
-	})
-	emps = append(emps, employee{
-		name:   "Raju",
-		basic:  45000,
-		otRate: 700,
-		otHrs:  40,
-	})
-	emps = append(emps, employee{
-		name:   "ko",
-		basic:  500000,
-		otRate: 700,
-		otHrs:  20,
-	})
-	emps = append(emps, employee{
-		name:   "roja",
-		basic:  60000,
-		otRate: 600,
-		otHrs:  30,
-	})
-	emps = append(emps, employee{
-		name:   "konara",
-		basic:  25000,
-		otRate: 700,
-		otHrs:  40,
-	})
-	emps = append(emps, employee{
-		name:   "Saman",
-		basic:  100000,
-		otRate: 500,
-		otHrs:  20,
-	})
-	emps = append(emps, employee{
-		name:   "Gasalu",
-		basic:  50000,
-		otRate: 300,
-		otHrs:  40,
-	})
-	emps = append(emps, employee{
-		name:   "Raju",
-		basic:  45000,
-		otRate: 700,
-		otHrs:  40,
-	})
-	emps = append(emps, employee{
-		name:   "ko",
-		basic:  500000,
-		otRate: 700,
-		otHrs:  20,
-	})
-	emps = append(emps, employee{
-		name:   "roja",
-		basic:  60000,
-		otRate: 600,
-		otHrs:  30,
-	})
-	emps = append(emps, employee{
-		name:   "konara",
-		basic:  25000,
-		otRate: 700,
-		otHrs:  40,
-	})
-	emps = append(emps, employee{
-		name:   "Saman",
-		basic:  100000,
-		otRate: 500,
-		otHrs:  20,
+		otHrs:  10,
 	})
 	emps = append(emps, employee{
 		name:   "Gasalu",
@@ -280,17 +62,21 @@ func main() {
 	})
 
 	for _, em := range emps {
-		otAmount := calOTAmount(em.otRate, em.otHrs)
-		fullSalary := calfullSalary(em.basic, otAmount)
-		fmt.Println(em.name, "Full Salary: ", fullSalary)
 
+		otAmCh := make(chan int)
+		fulSal := make(chan int)
+
+		go calOTAmount(em.otRate, em.otHrs, otAmCh)
+		go calfullSalary(em.basic, <-otAmCh, fulSal)
+
+		//otAmount := calOTAmount(em.otRate, em.otHrs)
+		//fullSalary := calfullSalary(em.basic, otAmount)
+
+		fmt.Println(em.name, "Full Salary: ", <-fulSal)
+		//fmt.Println(em.name, "Full Salary: ",fullSalary)
 		//time.Sleep(1 * time.Second)
-
 	}
 	endTime := time.Now()
 	diff := endTime.Sub(startTime)
 	fmt.Println("total time taken ", diff.Nanoseconds(), "Nanoseconds")
-	fmt.Println("======================================")
-	fmt.Println("Average time taken '130000 - 200000' Nanoseconds")
-
 }
